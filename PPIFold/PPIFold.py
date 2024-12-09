c81118b55c76053cde9ad982c6a380947de8ccab
Quentin Rouger <144003341+Qrouger@users.noreply.github.com>
2024-12-09 13:24:21 +0100

    Update PPIFold.py

--- PPIFold/PPIFold.py
+++ PPIFold/PPIFold.py
@@ -4,8 +4,8 @@
 """
 import argparse
 
-from . import utils
-from . import File_proteins
+from .utils import *
+from .File_proteins import *
 import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'AlphaPulldown')))
