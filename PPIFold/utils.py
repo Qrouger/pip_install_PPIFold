3af1916e263a8877fc662743c07197b22e4c1144
Quentin Rouger <144003341+Qrouger@users.noreply.github.com>
2025-01-09 10:01:45 +0100

    Update utils.py

--- PPIFold/utils.py
+++ PPIFold/utils.py
@@ -357,7 +357,7 @@ def Make_homo_oligo (data_dir, Path_Pickle_Feature) :
     Returns:
     ----------
     """
-    cmd =f"run_multimer_jobs.py --mode=homo-oligomer \--output_path=result_homo_oligo \--num_cycle=3 \--compress_result_pickles=True \--oligomer_state_file=homo_oligo.txt \--monomer_objects_dir={Path_Pickle_Feature} \--data_dir={data_dir} \--remove_result_pickles=False"
+    cmd =f"run_multimer_jobs.py --mode=custom \--output_path=result_homo_oligo \--num_cycle=3 \--compress_result_pickles=True \--oligomer_state_file=homo_oligo.txt \--monomer_objects_dir={Path_Pickle_Feature} \--data_dir={data_dir} \--remove_result_pickles=False"
     os.system(cmd)
 
 def add_hiQ_score (dir_alpha) :
